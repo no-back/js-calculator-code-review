e143cb788b46b63935c3a3b7c712c62be0fd1957
Eunyeol <ney9083@gmail.com>
2022-03-13 21:23:21 +0900

    Test: Calculator 기본 Test 코드 작성

---
+++ src/js/components/Digits.js
@@ -0,0 +1,15 @@
+class Digits {
+  constructor({ element, onClick }) {
+    this.element = element;
+    this.bindEvents(onClick);
+  }
+
+  bindEvents(onClick) {
+    this.element.addEventListener("click", ({ target }) => {
+      if (target.className !== "digit") return;
+      onClick(target.textContext);
+    });
+  }
+}
+
+export default Digits;
